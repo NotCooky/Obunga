f1b0bb56121da62be485e2f8cce788b4aebf6f1e
NotCooky <igabdullin2007@gmail.com>
2021-11-03 13:06:22 +0200

    new cool stuff!!!!!
    Reorganized project, redid the player movement script, added a basic gun script (does nothing for now), added jumping (bam make your own scene stop touching mine)

---
+++ Obunga-unity/Assets/Scripts/Gun.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    float damage = 10f;
+    float range = 100f;
+    
+    public Camera playerCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
---
+++ Obunga-unity/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // movement
+    float moveSpeed = 6f;
+    float jumpForce = 9f;
+    public float movementMultiplier = 10f;
+    float airMultiplier = 0.4f;
+
+    float horizontalMovement;
+    float verticalMovement;
+
+    //jumping???!?!?!
+    float playerHeight = 2f;
+    bool isGrounded;
+
+    //Drag
+    float groundDrag = 6f;
+    float airDrag = 2f;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 + 0.1f);
+        MyInput();
+        ControlDrag();
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void MyInput()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+
+        moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+    }
+
+    void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void ControlDrag()
+    {
+        if(isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
+        
+    }
+
+  
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        else if (!isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier, ForceMode.Acceleration);
+        }
+        
+    }
+}
