30d9be652016d3f7ba0e0f221d6c7a9f9b5ee2c5
NotCooky <igabdullin2007@gmail.com>
2021-11-04 20:01:40 +0200

    gun ammo system (wip), player crouching

--- Obunga-unity/Assets/Scripts/Gun.cs
+++ Obunga-unity/Assets/Scripts/Gun.cs
@@ -1,37 +1,78 @@
 ﻿
 using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
     public float range = 100f;
     public float shootStrength;
+    public int maxAmmo = 7;
+    private int currentAmmo;
+    public float reloadTime = 0.5f;
+    private bool isReloading;
+    public float fireRate;
+
     public ParticleSystem muzzleFlash;
-    public Animation shootAnim;
+    public Animator animator;
     
     public Camera playerCam;
 
+    private float nextTimeToFire = 0f;
+
     void Start()
     {
-       
+       currentAmmo = maxAmmo;
+    }
+
+   
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("reloading", false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
-        if(Input.GetMouseButton(0))
+        if(isReloading)
         {
-            muzzleFlash.Play();
-            shootAnim.Play();
+            return;
+        }
+
+        if(currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if(Input.GetMouseButton(0) && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
             
             
         }
     }
 
+      IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Realod");
+        animator.SetBool("reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+        animator.SetBool("reloading", false);
+        yield return new WaitForSeconds(.25f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     void Shoot()
     {
+        currentAmmo--;
+        muzzleFlash.Play();
+
         RaycastHit hit;
         if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
         {
@@ -41,4 +82,6 @@ public class Gun : MonoBehaviour
             
         }
     }
+
+  
 }
--- Obunga-unity/Assets/Scripts/PlayerMove.cs
+++ Obunga-unity/Assets/Scripts/PlayerMove.cs
@@ -6,6 +6,7 @@ public class PlayerMove : MonoBehaviour
 {
     // movement
     float moveSpeed = 6f;
+    float crouchSpeed = 1f;
     
     float jumpForce = 5f;
     public float movementMultiplier = 10f;
@@ -26,6 +27,7 @@ public class PlayerMove : MonoBehaviour
     Vector3 crouchScale = new Vector3(1, 0.5f, 1);
     Vector3 playerScale;
     Vector3 moveDirection;
+    float wantedHeight;
     bool isCrouching;
 
     Rigidbody rb;
@@ -41,11 +43,13 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
+        playercol.height = Mathf.Lerp(playercol.height, wantedHeight, Time.deltaTime * crouchSpeed);
+
         isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 + 0.1f);
         MyInput();
         ControlDrag();
 
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded && !isCrouching)
         {
             Jump();
         }
@@ -77,16 +81,18 @@ public class PlayerMove : MonoBehaviour
 
     void Crouch()
     {
-        transform.localScale = crouchScale;
-        transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+       // transform.localScale = crouchScale;
+        //transform.position = new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z);
+        wantedHeight = 1f;
         isCrouching = true;
         
     }
 
     void Uncrouch()
     {
-        transform.localScale = playerScale;
-        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+       // transform.localScale = playerScale;
+       // transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+       wantedHeight = 2f;
         isCrouching = false;
     }
 
