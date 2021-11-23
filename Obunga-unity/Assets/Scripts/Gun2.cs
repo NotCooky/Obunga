f557ca45527470cc3d7902b5900f2089f843b0e8
Cokunga <igabdullin2007@gmail.com>
2021-11-23 17:42:27 +0200

    added jerry

--- Obunga-unity/Assets/Scripts/Gun2.cs
+++ Obunga-unity/Assets/Scripts/Gun2.cs
@@ -6,6 +6,7 @@ public class Gun2 : MonoBehaviour
 {
     float damage = 10f;
     float range = 100f;
+    float impactForce = 30f;
 
     public Camera playerCam;
     public ParticleSystem muzzleFlash;
@@ -38,6 +39,10 @@ public class Gun2 : MonoBehaviour
         if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, range))
         {
             //Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+           if (hit.rigidbody != null)
+           {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+           }
         }
     }
 }
