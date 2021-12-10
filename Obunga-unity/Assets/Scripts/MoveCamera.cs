8898ba3e9cd9a9cca0ad1ff5fbd313a1f5e4a444
Cokunga <igabdullin2007@gmail.com>
2021-12-10 20:25:04 +0200

    misc improvements

--- Obunga-unity/Assets/Scripts/MoveCamera.cs
+++ Obunga-unity/Assets/Scripts/MoveCamera.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour {
 
     public Transform cameraPos;
-
+    public Vector3 vaultOffset;
     void Update() 
     {
-        transform.position = cameraPos.position;
+        transform.position = cameraPos.position + vaultOffset;
+        vaultOffset = Vector3.Slerp(vaultOffset, Vector3.zero, Time.deltaTime * 3f);
     }
 }
